5988ccf923a4c9c39b76df296a0794223ad506fd
Maria Vera <maria.vera5@correo.uis.edu.co>
2020-10-26 14:01:58 -0500

    V 1.2

--- routes.js
+++ routes.js
@@ -1,10 +1,30 @@
 'use strict'
 const bodyParser = require('body-parser');
 const express = require('express');
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-const controlador1=require('./controladores/controllerFormularioLegalizacion');
-const controlador2=require('./controladores/controllerProgramaAcademico');
-const controlador3=require('./controladores/controllerSuma');
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
+})
+const controlador1 = require('./controladores/controllerFormularioLegalizacion');
+const controlador2 = require('./controladores/controllerProgramaAcademico');
+const controlador3 = require('./controladores/controllerSuma');
+
+//semaphore.js 
+//https://www.npmjs.com/package/semaphore
+//Limita el acceso simultaneo a un recurso
+//ejemplo:
+// Limit concurrent db access
+// var sem = require('semaphore')(1);
+// var server = require('http').createServer(req, res) {
+//     sem.take(function() {
+//         expensive_database_operation(function(err, res) {
+//             sem.leave();
+ 
+//             if (err) return res.end("Error");
+ 
+//             return res.end(res);
+//         });
+//     });
+// });
 var sem = require('semaphore')(1);
 var sem2 = require('semaphore')(1);
 
@@ -12,57 +32,44 @@ var sem2 = require('semaphore')(1);
 
 var api = express.Router();
 api.post('/FormularioLegalizacion', urlencodedParser,
-(req,res)=>{
-    console.log('primero');
-    sem.take(async function() {
-       await controlador1(req,res);
-    
-    setTimeout(() => {
+    (req, res) => {
+        sem.take(async function () {
+            await controlador1(req, res);
 
-        sem.leave();
-
-
-            console.log('Primer TimeOut');
-            
-        }, 10000);
-    });
-}
-);
- 
-api.post('/ProgramaAcademico',urlencodedParser, 
-(req,res)=>{
-    console.log('segundo');
-
-    sem.take(async function() {
-   
-await controlador2(req,res );
-    setTimeout(() => {
-        sem.leave();
-
-
-            console.log('second TimeOut');
-            
-        }, 10000);
-    });
-}
+            setTimeout(() => {
+                console.log('Primer TimeOut');
+                sem.leave();
+            }, 10000);
+        });
+    }
 );
 
-api.post('/Suma',urlencodedParser, 
-(req,res)=>{
-    console.log('Tercero');
+api.post('/ProgramaAcademico', urlencodedParser,
+    (req, res) => {
 
-    sem2.take(async function() {
-   
-await controlador3(req,res );
-    setTimeout(() => {
-        sem2.leave();
+        sem.take(async function () {
 
+            await controlador2(req, res);
+            setTimeout(() => {
+                console.log('second TimeOut');
+                sem.leave();
+            }, 10000);
+        });
+    }
+);
 
-            console.log('third TimeOut');
-            
-        }, 3000);
-    });
-}
+api.post('/Suma', urlencodedParser,
+    (req, res) => {
+
+        sem2.take(async function () {
+
+            await controlador3(req, res);
+            setTimeout(() => {
+                console.log('third TimeOut');
+                sem2.leave();
+            }, 3000);
+        });
+    }
 );
 // api.post('/ProgramaAcademico2',urlencodedParser,require('./controladores/controllerProgramaAcademico'));
 
